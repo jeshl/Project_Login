80a45c53aac1fd70e0bb1e16d3e0a75231702954
jesh <jeshlimbu932@gmail.com>
2023-06-13 21:05:43 +0545

    Login-1_13-06

---
+++ Login/app/src/main/java/com/example/login/Login_jav.java
@@ -0,0 +1,37 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login_jav extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(Login_jav.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Login_jav.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+}
